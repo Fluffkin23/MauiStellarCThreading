fdc86399e4c2ad55d5c804f51164e2e2774598fb
lele.husarescu1@student.nhlstenden.com <lele.husarescu1@student.nhlstenden.com>
2024-05-14 16:36:16 +0200

    Creating the model for horoscope

--- MauiStellarCThreading/Model/Horoscope.cs
+++ MauiStellarCThreading/Model/Horoscope.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace MauiStellarCThreading.Model
 {
-    internal class Horoscope
+    public class Horoscope
     {
+        public bool Status { get; set; }
+        public string Prediction { get; set; }
+        public string Number { get; set; }
+        public string Color { get; set; }
+        public string Mantra { get; set; }
+        public string Remedy { get; set; }
     }
 }
