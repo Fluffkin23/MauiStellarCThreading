8ed17cfd75d4a5ca63c77f174292667a36964e57
Costache Florian-Alin <91316565+Fluffkin23@users.noreply.github.com>
2024-05-14 14:00:05 +0200

    Add project files.

---
+++ MauiStellarCThreading/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiStellarCThreading
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
