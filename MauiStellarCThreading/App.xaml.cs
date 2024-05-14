c0d83d800d3ae58f6a1b68a16a3f0f4c609c2e2e
Costache Florian-Alin <91316565+Fluffkin23@users.noreply.github.com>
2024-05-14 16:52:50 +0200

    Adding fix size and make the app not resizable

--- MauiStellarCThreading/App.xaml.cs
+++ MauiStellarCThreading/App.xaml.cs
@@ -8,5 +8,25 @@
 
             MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 500;
+            const int newHeight = 800;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            window.MinimumHeight = newHeight;
+            window.MinimumWidth = newWidth;
+
+            window.MaximumHeight = newHeight;
+            window.MaximumWidth = newWidth;
+
+            return window;
+
+        }
     }
 }
